ac5894c4a170322aa4cbcb4d5edb4264b759e72f
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-26 10:38:50 -0700

    added javascript

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 var input = document.getElementById("input");
 var text = document.querySelector(".text");
+
+function addTask() {
+    if (!input.value.trim()) {
+         alert("Todo can't be empty!");
+    } else {
+        var li = document.createElement("li");
+        li.textContent = input.value;
+
+        var removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.className = 'remove-btn';
+        removeButton.onclick = function () {
+            text.removeChild(li); 
+        };
+
+        li.appendChild(removeButton);
+        text.appendChild(li); 
+        input.value = ""; 
+    }
+}
